1402add66723fe3c0ad8844245fa05279130a4d0
andrisyahputra <andrisyahputra2209@gmail.com>
2023-05-23 13:03:10 +0700

    update di dashboard tambah div

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -501,8 +501,11 @@
                 <!-- Content wrapper -->
                 <div class="content-wrapper">
                     <!-- Content -->
+                    <div class="container-xxl flex-grow-1 container-p-y">
+
                     @yield('content')
                     
+                  </div>
                     <!-- / Content -->
 
                     <!-- Footer -->

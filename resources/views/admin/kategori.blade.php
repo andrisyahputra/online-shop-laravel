ef34b615c35579cceb6e49c81a8e9990df3782fa
andrisyahputra <andrisyahputra2209@gmail.com>
2023-05-27 13:33:58 +0700

    pertemuan 7 bahas ajax 2

---
+++ resources/views/admin/kategori.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.dashboard')
+@section('content')
+{{-- Uji coba --}}
+<div class="card">
+    <div class="card-body">
+
+        <div class="d-flex mb-4">
+            <button class="btn btn-primary ms-auto me-0" data-bs-toggle="modal" data-bs-target="#add_category"> <i class="bi bi-plus-lg"></i>
+                Tambah Kategori
+            </button>
+        </div>
+
+        <table class="table table-bordered" id="myTable">
+            <thead>
+                <tr>
+                    <th scope="col">Column 1</th>
+                    <th scope="col">Column 2</th>
+                    <th scope="col">Column 3</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="">
+                    <td scope="row">R1C1</td>
+                    <td>R1C2</td>
+                    <td>R1C3</td>
+                </tr>
+                <tr class="">
+                    <td scope="row">Item</td>
+                    <td>Item</td>
+                    <td>Item</td>
+                </tr>
+            </tbody>
+        </table>
+
+
+    </div>
+</div>
+
+<!-- Modal Body -->
+<!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+<div class="modal fade" id="add_category" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+    aria-labelledby="modalTitleId" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
+        <div class="modal-content">
+            <form action="{{ route('kategori.store') }}" method="POST" id="form">
+                @csrf
+
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalTitleId">Tambah Kategori</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <label for="" class="form-label">Nama Kategori</label>
+                    <input type="text" class="form-control" name="nama" placeholder="Masukkan Nama Kategori">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" onclick="send_form('#form')" id="btn_submit"> <i class="bi bi-files"></i> Simpan</button>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
+
+@endsection
